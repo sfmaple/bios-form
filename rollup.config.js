61feb9ec8d762361a21485f6f1958127b6183352
sfmaple <sfmapleleaf@gmail.com>
2019-04-24 11:55:55 +0800

    refactor: replace rollup with gulp

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import ts from 'typescript';
+import tsPlugin from 'rollup-plugin-typescript';
+import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import sourceMaps from "rollup-plugin-sourcemaps";
+import {
+  uglify
+} from 'rollup-plugin-uglify'
+
+const NODE_ENV = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const extensions = [".ts", ".tsx", ".js", ".jsx"]
+const external = [
+  'react', 'axios', 'mitt',
+  'lodash.get', 'lodash.set', 'lodash.pick',
+  'lodash.omit', 'lodash.clonedeep'
+]
+
+const plugins = [
+  replace({
+    'process.env.NODE_ENV': NODE_ENV
+  }),
+  sourceMaps(),
+  nodeResolve({
+    extensions
+  }),
+  commonjs({
+    include: 'node_modules/**'
+  }),
+  tsPlugin({
+    typescript: ts,
+    importHelpers: true
+  })
+]
+
+if (NODE_ENV === 'production') {
+  plugins.push(uglify());
+}
+
+export default {
+  plugins,
+  external,
+  input: './src/index.ts',
+  output: {
+    file: './dist/index.js',
+    format: 'cjs'
+  }
+}
