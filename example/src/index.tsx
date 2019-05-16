3be38fde77a0e6648a0cc21145c35bd97f53ab30
sfmaple (from Travis CI) <sfmapleleaf@gmail.com>
2019-05-16 07:25:35 +0000

    Deploy sfmaple/tea-form to github.com/sfmaple/tea-form.git:gh-pages

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import omit from 'lodash.omit';
 import { Input, Button } from 'antd';
-import SchemaForm from 'tea-form';
+import SchemaForm from 'bios-form';
 import './index.css';
 
 class FInput extends React.Component<any> {
